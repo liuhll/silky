f08d2dee546b4f6a9b6fb589af1d796aa938efec
liuhll <1029765111@qq.com>
2021-02-08 21:18:02 +0800

    更新rpc端口号缺省配置

--- framework/src/Lms.Rpc/Configuration/RpcOptions.cs
+++ framework/src/Lms.Rpc/Configuration/RpcOptions.cs
@@ -5,8 +5,8 @@ namespace Lms.Rpc.Configuration
         public static string Rpc = "Rpc";
 
         public string Host { get; set; } = "0.0.0.0";
-        public int RpcPort { get; set; } = 100;
-        public int MqttPort { get; set; } = 200;
+        public int RpcPort { get; set; } = 2200;
+        public int MqttPort { get; set; } = 2300;
         public bool UseLibuv { get; set; } = true;
         public bool IsSsl { get; set; } = false;
 
