9d61d034905f704629f3b0bb773b17a8e782dd5d
liuhll <1029765111@qq.com>
2021-03-16 23:07:14 +0800

    新增Lms.NormHost组件包

---
+++ framework/src/Lms.NormHost/NormHostModule.cs
@@ -0,0 +1,19 @@
+﻿using Lms.Codec;
+using Lms.Core.Modularity;
+using Lms.DotNetty.Protocol.Tcp;
+using Lms.RegistryCenter.Zookeeper;
+using Lms.Rpc.Proxy;
+using Lms.Transaction.Tcc;
+
+namespace Lms.NormHost
+{
+    [DependsOn(typeof(ZookeeperModule),
+        typeof(DotNettyTcpModule),
+        typeof(MessagePackModule),
+        typeof(RpcProxyModule),
+        typeof(TransactionTccModule)
+    )]
+    public class NormHostModule : LmsModule
+    {
+    }
+}
