7ba72adc429feadd5c1fb7ac0e27a63c7b33919c
liuhll <1029765111@qq.com>
2021-04-02 23:42:18 +0800

    修复获取UnitofWork存在的bug

--- samples/microservices/account/Lms.Account.Domain/Accounts/AccountDomainService.cs
+++ samples/microservices/account/Lms.Account.Domain/Accounts/AccountDomainService.cs
@@ -2,28 +2,18 @@ using System.Threading.Tasks;
 using Arch.EntityFrameworkCore.UnitOfWork;
 using Lms.Account.Application.Contracts.Accounts.Dtos;
 using Lms.AutoMapper;
+using Lms.Core;
 using Lms.Core.Exceptions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Lms.Account.Domain.Accounts
 {
     public class AccountDomainService : IAccountDomainService
     {
-        // private readonly IRepository<Account> _accountRepository;
-        //
-        // public AccountDomainService(IRepository<Account> accountRepository)
-        // {
-        //     _accountRepository = accountRepository;
-        // }
-        private readonly IUnitOfWork _unitOfWork;
-
-        public AccountDomainService(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
-
         public async Task<Account> Create(Account account)
         {
-            var accountRepository = _unitOfWork.GetRepository<Account>();
+            using var unitOfWork = GetUnitOfWork();
+            var accountRepository = unitOfWork.GetRepository<Account>();
             var exsitAccountCount = accountRepository.Count(p => p.Name == account.Name);
             if (exsitAccountCount > 0)
             {
@@ -35,41 +25,71 @@ namespace Lms.Account.Domain.Accounts
             {
                 throw new BusinessException($"已经存在{account.Email}Email的账号");
             }
+
             var insertResult = await accountRepository.InsertAsync(account);
-            await _unitOfWork.SaveChangesAsync();
+            await unitOfWork.SaveChangesAsync();
             return insertResult.Entity;
         }
 
         public async Task<Account> GetAccountByName(string name)
         {
-            var accountRepository = _unitOfWork.GetRepository<Account>();
-            var accountEntry = await accountRepository.GetFirstOrDefaultAsync(predicate: p=> p.Name == name);
+            using var unitOfWork = GetUnitOfWork();
+            var accountRepository = unitOfWork.GetRepository<Account>();
+            var accountEntry = await accountRepository.GetFirstOrDefaultAsync(predicate: p => p.Name == name);
             if (accountEntry == null)
             {
                 throw new BusinessException($"不存在名称为{name}的账号");
             }
+
             return accountEntry;
         }
 
         public async Task<Account> GetAccountById(long id)
         {
-            var accountRepository = _unitOfWork.GetRepository<Account>();
-            var accountEntry = await accountRepository.GetFirstOrDefaultAsync(predicate: p=> p.Id == id);
+            using var unitOfWork = GetUnitOfWork();
+            var accountRepository = unitOfWork.GetRepository<Account>();
+            var accountEntry = await accountRepository.GetFirstOrDefaultAsync(predicate: p => p.Id == id);
             if (accountEntry == null)
             {
                 throw new BusinessException($"不存在Id为{id}的账号");
             }
+
             return accountEntry;
         }
 
         public async Task<Account> Update(UpdateAccountInput input)
         {
             var account = await GetAccountById(input.Id);
+            using var unitOfWork = GetUnitOfWork();
+            var accountRepository = unitOfWork.GetRepository<Account>();
+            if (!account.Email.Equals(input.Email))
+            {
+                var exsitAccountCount = accountRepository.Count(p => p.Email == input.Email);
+                if (exsitAccountCount > 0)
+                {
+                    throw new BusinessException($"系统中已经存在Email为{input.Email}的账号");
+                }
+            }
+
+            if (!account.Name.Equals(input.Name))
+            {
+                var exsitAccountCount = accountRepository.Count(p => p.Name == input.Name);
+                if (exsitAccountCount > 0)
+                {
+                    throw new BusinessException($"系统中已经存在Name为{input.Name}的账号");
+                }
+            }
+
             account = input.MapTo(account);
-            var accountRepository = _unitOfWork.GetRepository<Account>(); 
+
             accountRepository.Update(account);
-            await _unitOfWork.SaveChangesAsync();
+            await unitOfWork.SaveChangesAsync();
             return account;
+        }
+
+        private IUnitOfWork GetUnitOfWork()
+        {
+            return EngineContext.Current.ServiceProvider.CreateScope().ServiceProvider.GetService<IUnitOfWork>();
         }
     }
 }
