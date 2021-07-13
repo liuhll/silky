0d381cb2a1d4a4a24c81e59fea53dcda8869e493
liuhll <1029765111@qq.com>
2021-07-13 17:57:23 +0800

    新增工作单元

---
+++ framework/src/Silky.Lms.EntityFrameworkCore/UnitOfWork/Filters/EfCoreUnitOfWorkFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Silky.Lms.Core.DynamicProxy;
+using Silky.Lms.EntityFrameworkCore.ContextPools;
+
+namespace Silky.Lms.EntityFrameworkCore.UnitOfWork
+{
+    public class EfCoreUnitOfWorkFilter
+    {
+        private readonly IDbContextPool _dbContextPool;
+
+        public EfCoreUnitOfWorkFilter(IDbContextPool dbContextPool)
+        {
+            _dbContextPool = dbContextPool;
+        }
+
+        public async Task InterceptAsync(ILmsMethodInvocation invocation)
+        {
+        }
+    }
+}
