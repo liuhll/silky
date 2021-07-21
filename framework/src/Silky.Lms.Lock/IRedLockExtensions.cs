4de4ab95b6589dd60b823844d151d667da7587e4
刘洪亮 <liuhongliang@jingshonline.com>
2021-07-21 14:05:02 +0800

    修复获取分布式锁失败存在的bug

--- framework/src/Silky.Lms.Lock/IRedLockExtensions.cs
+++ framework/src/Silky.Lms.Lock/IRedLockExtensions.cs
@@ -6,6 +6,7 @@ using Silky.Lms.Core.Exceptions;
 using Microsoft.Extensions.Options;
 using RedLockNet;
 using Silky.Lms.Lock.Configuration;
+using Silky.Lms.Lock.Provider;
 
 namespace Silky.Lms.Lock
 {
@@ -15,36 +16,56 @@ namespace Silky.Lms.Lock
         {
             var lockOptions = EngineContext.Current.Resolve<IOptions<LockOptions>>().Value;
             var lockerTime = DateTime.Now;
-            while (true) 
+            try
             {
-                if (redLock.IsAcquired) 
+                while (true)
                 {
-                    await callback();
-                    break;
-                }
-                Thread.Sleep(lockOptions.Retry);
-                if (DateTime.Now - lockerTime > lockOptions.DefaultExpiryTimeSpan) 
-                {
-                    throw new LmsException($"获取分布式锁资源{redLock.Resource}超时");
+                    if (redLock.IsAcquired)
+                    {
+                        await callback();
+                        break;
+                    }
+
+                    redLock.Dispose();
+                    Thread.Sleep(lockOptions.Retry);
+                    redLock = await EngineContext.Current.Resolve<ILockerProvider>().CreateLockAsync(redLock.Resource);
+                    if (DateTime.Now - lockerTime > lockOptions.DefaultExpiryTimeSpan)
+                    {
+                        throw new LmsException($"获取分布式锁资源{redLock.Resource}超时");
+                    }
                 }
             }
+            finally
+            {
+               redLock?.Dispose();
+            }
+           
         }
 
         public static async Task<T> Lock<T>(this IRedLock redLock, Func<Task<T>> callback)
         {
             var lockerTime = DateTime.Now;
             var lockOptions = EngineContext.Current.Resolve<IOptions<LockOptions>>().Value;
-            while (true)
+            try
             {
-                if (redLock.IsAcquired)
+                while (true)
                 {
-                    return await callback();
+                    if (redLock.IsAcquired)
+                    {
+                        return await callback();
+                    }
+                    redLock.Dispose();
+                    Thread.Sleep(lockOptions.Retry);
+                    redLock = await EngineContext.Current.Resolve<ILockerProvider>().CreateLockAsync(redLock.Resource);
+                    if (DateTime.Now - lockerTime > lockOptions.DefaultExpiryTimeSpan)
+                    {
+                        throw new LmsException($"获取分布式锁资源{redLock.Resource}超时");
+                    }
                 }
-                Thread.Sleep(lockOptions.Retry);
-                if (DateTime.Now - lockerTime > lockOptions.DefaultExpiryTimeSpan)
-                {
-                    throw new LmsException($"获取分布式锁资源{redLock.Resource}超时");
-                }
+            }
+            finally
+            {
+                redLock?.Dispose();
             }
         }
     }
