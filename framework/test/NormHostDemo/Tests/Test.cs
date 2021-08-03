e0cab3a9841f100f0636968fcbfdb6bb2da3c0c0
liuhll <1029765111@qq.com>
2021-08-03 21:20:47 +0800

    测试通过种子数据初始化数据

--- framework/test/NormHostDemo/Tests/Test.cs
+++ framework/test/NormHostDemo/Tests/Test.cs
@@ -1,9 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 using Silky.EntityFrameworkCore.Entities;
+using Silky.EntityFrameworkCore.Entities.Configures;
 
 namespace NormHostDemo.Tests
 {
-    public class Test : IEntity
+    public class Test : IEntity, IEntitySeedData<Test>
     {
         [Key]
         public long Id { get; set; }
@@ -11,5 +15,17 @@ namespace NormHostDemo.Tests
         public string Name { get; set; }
 
         public string Address { get; set; }
+        public IEnumerable<Test> HasData(DbContext dbContext, Type dbContextLocator)
+        {
+            return new List<Test>()
+            {
+                new ()
+                {
+                    Id = 1,
+                    Name = "test",
+                    Address = "beijing"
+                },
+            };
+        }
     }
 }
