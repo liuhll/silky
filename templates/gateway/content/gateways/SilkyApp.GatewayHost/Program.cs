792b63f05d2276be3042d4862707c95504a34142
liuhongliang <1029765111@qq.com>
2021-08-15 22:44:54 +0800

    优化模板

--- templates/gateway/content/gateways/SilkyApp.GatewayHost/Program.cs
+++ templates/gateway/content/gateways/SilkyApp.GatewayHost/Program.cs
@@ -5,9 +5,9 @@ namespace SilkyApp.GatewayHost
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args)
