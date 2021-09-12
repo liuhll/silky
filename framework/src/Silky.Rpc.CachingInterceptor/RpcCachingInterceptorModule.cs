ce10d5c95d5453683e6b1918878b05b4670c89ae
liuhll <1029765111@qq.com>
2021-09-13 00:36:48 +0800

    Rpc缓存拦截模块需要依赖CachingModule

--- framework/src/Silky.Rpc.CachingInterceptor/RpcCachingInterceptorModule.cs
+++ framework/src/Silky.Rpc.CachingInterceptor/RpcCachingInterceptorModule.cs
@@ -1,11 +1,12 @@
 using Autofac;
+using Silky.Caching;
 using Silky.Castle;
 using Silky.Core.Modularity;
 using Silky.Rpc.Runtime;
 
 namespace Silky.Rpc.CachingInterceptor
 {
-    [DependsOn(typeof(RpcModule))]
+    [DependsOn(typeof(RpcModule), typeof(CachingModule))]
     public class RpcCachingInterceptorModule : SilkyModule
     {
         protected override void RegisterServices(ContainerBuilder builder)
