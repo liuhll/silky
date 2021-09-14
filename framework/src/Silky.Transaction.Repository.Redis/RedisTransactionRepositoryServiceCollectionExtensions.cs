8276493df62488b6b67f9ce6fe64775ca34616d2
liuhll <1029765111@qq.com>
2021-09-14 09:34:04 +0800

    优化RedisTransactionRepositoryServiceCollectionExtensions

--- framework/src/Silky.Transaction.Repository.Redis/RedisTransactionRepositoryServiceCollectionExtensions.cs
+++ framework/src/Silky.Transaction.Repository.Redis/RedisTransactionRepositoryServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@ using Medallion.Threading;
 using Medallion.Threading.Redis;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
 using Silky.Caching.StackExchangeRedis;
+using Silky.Core;
 using Silky.Core.DependencyInjection;
 using Silky.Transaction.Abstraction;
 using Silky.Transaction.Repository.Redis;
@@ -12,9 +13,10 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class RedisTransactionRepositoryServiceCollectionExtensions
     {
         public static IServiceCollection AddRedisTransactionRepository(this IServiceCollection services,
-            RedisCacheOptions redisCacheOption)
+            RedisCacheOptions redisCacheOption, int lockStoreDbIndex = 15)
 
         {
+            Check.NotNull(redisCacheOption, nameof(redisCacheOption));
             if (!services.IsAddedImplementationType(typeof(SilkyRedisCache)))
             {
                 services.AddRedisCaching(redisCacheOption);
@@ -22,7 +24,8 @@ namespace Microsoft.Extensions.DependencyInjection
 
             services.AddSingleton(typeof(ITransRepository), typeof(RedisTransRepository));
             var connection = ConnectionMultiplexer.Connect(redisCacheOption.Configuration);
-            var distributedLockProvider = new RedisDistributedSynchronizationProvider(connection.GetDatabase(15));
+            var distributedLockProvider =
+                new RedisDistributedSynchronizationProvider(connection.GetDatabase(lockStoreDbIndex));
             services.AddSingleton<IDistributedLockProvider>(distributedLockProvider);
             return services;
         }
