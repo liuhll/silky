c5d0ee3a920ab397147cf4d235bea6edb2339509
liuhll <1029765111@qq.com>
2021-09-15 15:00:50 +0800

    删除不必要的代码

--- framework/src/Silky.Codec.MessagePack/MessagePackModule.cs
+++ framework/src/Silky.Codec.MessagePack/MessagePackModule.cs
@@ -8,12 +8,6 @@ namespace Silky.Codec
     [DependsOn(typeof(RpcModule))]
     public class MessagePackModule : SilkyModule
     {
-        // protected override void RegisterServices(ContainerBuilder builder)
-        // {
-        //     builder.RegisterType<MessagePackTransportMessageDecoder>().AsSelf().AsImplementedInterfaces().InstancePerDependency();
-        //     builder.RegisterType<MessagePackTransportMessageEncoder>().AsSelf().AsImplementedInterfaces().InstancePerDependency();
-        // }
-
         public override void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
             services.AddMessagePackCodec();
