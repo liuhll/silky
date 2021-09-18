a79a590084008ffffea4299a91f8b8d89ba4ae6b
liuhll <1029765111@qq.com>
2021-09-18 19:11:26 +0800

    移除不必要模块依赖判断

--- framework/src/Silky.DotNetty.Protocol.Tcp/DotNettyTcpModule.cs
+++ framework/src/Silky.DotNetty.Protocol.Tcp/DotNettyTcpModule.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Autofac;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using Silky.Core;
-using Silky.Core.Exceptions;
 using Silky.Core.Modularity;
 using Silky.Rpc;
-using Silky.Rpc.Configuration;
-using Silky.Rpc.Extensions;
 using Silky.Rpc.Routing;
-using Silky.Rpc.Runtime.Server;
 
 namespace Silky.DotNetty.Protocol.Tcp
 {
@@ -30,16 +21,11 @@ namespace Silky.DotNetty.Protocol.Tcp
 
         public override async Task Initialize(ApplicationContext applicationContext)
         {
-            Check.NotNull(applicationContext, nameof(applicationContext));
-            if (!applicationContext.IsDependsOnRegistryCenterModule(out var registryCenterType))
-            {
-                throw new SilkyException(
-                    $"You did not specify the dependent {registryCenterType} service registry module");
-            }
-
-            var messageListener = applicationContext.ServiceProvider.GetRequiredService<DotNettyTcpServerMessageListener>();
+            var messageListener =
+                applicationContext.ServiceProvider.GetRequiredService<DotNettyTcpServerMessageListener>();
             await messageListener.Listen();
-            var serviceRouteRegisterProvider = applicationContext.ServiceProvider.GetRequiredService<IServiceRouteRegisterProvider>();
+            var serviceRouteRegisterProvider =
+                applicationContext.ServiceProvider.GetRequiredService<IServiceRouteRegisterProvider>();
             await serviceRouteRegisterProvider.RegisterTcpRoutes();
         }
     }
