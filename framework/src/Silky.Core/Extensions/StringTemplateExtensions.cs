d230db78e0dbf31d1b92029decf58d96e152b4e8
liuhll <1029765111@qq.com>
2021-10-14 20:16:07 +0800

    移除Clay模块代码

--- framework/src/Silky.Core/Extensions/StringTemplateExtensions.cs
+++ framework/src/Silky.Core/Extensions/StringTemplateExtensions.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using Silky.Core.ClayObject.Extensions;
 
 namespace Silky.Core.Extensions
 {
@@ -17,55 +15,8 @@ namespace Silky.Core.Extensions
         /// 读取配置模板正则表达式
         /// </summary>
         private const string configTemplatePattern = @"\#\((?<p>.*?)\)";
-
-        /// <summary>
-        /// 渲染模板
-        /// </summary>
-        /// <param name="template"></param>
-        /// <param name="templateData"></param>
-        /// <param name="encode"></param>
-        public static void Render(this string template, object templateData, bool encode = false)
-        {
-            if (template == null) return;
-
-            template.Render(templateData == null ? default : templateData.ToDictionary(), encode);
-        }
-
-        /// <summary>
-        /// 渲染模板
-        /// </summary>
-        /// <param name="template"></param>
-        /// <param name="templateData"></param>
-        /// <param name="encode"></param>
-        /// <returns></returns>
-        public static string Render(this string template, IDictionary<string, object> templateData, bool encode = false)
-        {
-            if (template == null) return default;
-
-            // 如果模板为空，则跳过
-            if (templateData == null || templateData.Count == 0) return template;
-
-            // 判断字符串是否包含模板
-            if (!Regex.IsMatch(template, commonTemplatePattern)) return template;
-
-            // 获取所有匹配的模板
-            var templateValues = Regex.Matches(template, commonTemplatePattern)
-                .Select(u => new
-                {
-                    Template = u.Groups["p"].Value,
-                    Value = MatchTemplateValue(u.Groups["p"].Value, templateData)
-                });
-
-            // 循环替换模板
-            foreach (var item in templateValues)
-            {
-                template = template.Replace($"{{{item.Template}}}",
-                    encode ? Uri.EscapeDataString(item.Value?.ToString() ?? string.Empty) : item.Value?.ToString());
-            }
-
-            return template;
-        }
-
+        
+        
         /// <summary>
         /// 从配置中渲染字符串模板
         /// </summary>
@@ -97,28 +48,6 @@ namespace Silky.Core.Extensions
             return template;
         }
 
-        /// <summary>
-        /// 匹配模板值
-        /// </summary>
-        /// <param name="template"></param>
-        /// <param name="templateData"></param>
-        /// <returns></returns>
-        private static object MatchTemplateValue(string template, IDictionary<string, object> templateData)
-        {
-            string tmpl;
-            if (!template.Contains(".", StringComparison.CurrentCulture)) tmpl = template;
-            else tmpl = template.Split('.', StringSplitOptions.RemoveEmptyEntries).First();
-
-            var templateValue = templateData.ContainsKey(tmpl) ? templateData[tmpl] : default;
-            return ResolveTemplateValue(template, templateValue);
-        }
-
-        /// <summary>
-        /// 解析模板的值
-        /// </summary>
-        /// <param name="template"></param>
-        /// <param name="data"></param>
-        /// <returns></returns>
         private static object ResolveTemplateValue(string template, object data)
         {
             // 根据 . 分割模板
