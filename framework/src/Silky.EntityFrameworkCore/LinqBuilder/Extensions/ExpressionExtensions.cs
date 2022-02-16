ecc6ef7ff688d1721a239554ba8d096361d97cfe
liuhll <1029765111@qq.com>
2022-02-17 00:38:56 +0800

    移除不必要的扩展方法

--- framework/src/Silky.EntityFrameworkCore/LinqBuilder/Extensions/ExpressionExtensions.cs
+++ framework/src/Silky.EntityFrameworkCore/LinqBuilder/Extensions/ExpressionExtensions.cs
@@ -164,16 +164,6 @@ namespace Silky.EntityFrameworkCore.LinqBuilder
 
             throw new InvalidCastException(nameof(expression));
         }
-
-        /// <summary>
-        /// 是否是空集合
-        /// </summary>
-        /// <typeparam name="TSource">泛型类型</typeparam>
-        /// <param name="sources">集合对象</param>
-        /// <returns>是否为空集合</returns>
-        public static bool IsNullOrEmpty<TSource>(this IEnumerable<TSource> sources)
-        {
-            return sources == null || !sources.Any();
-        }
+        
     }
 }
