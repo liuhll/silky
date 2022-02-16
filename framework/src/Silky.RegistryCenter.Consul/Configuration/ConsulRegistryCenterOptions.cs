5ef931d473c70d65b049ba1d65755b0514bf4b1b
Dishone <50564348+Dishone@users.noreply.github.com>
2022-02-17 04:26:52 +0800

    Added Consul configuration options
    Configure cannot be converted to Uri type It is recommended to add configuration options

--- framework/src/Silky.RegistryCenter.Consul/Configuration/ConsulRegistryCenterOptions.cs
+++ framework/src/Silky.RegistryCenter.Consul/Configuration/ConsulRegistryCenterOptions.cs
@@ -1,3 +1,4 @@
+using System;
 using Consul;
 using Silky.Rpc.Configuration;
 
@@ -8,7 +9,16 @@ namespace Silky.RegistryCenter.Consul.Configuration
         internal static string RegistryCenterSection = "RegistryCenter";
 
         internal const string SilkyServer = "SilkyServer";
+        
+        public string Host { get; set; } = "127.0.0.1";
 
+        public int Port { get; set; } = 8500;
+
+        public Uri Address
+        {
+            get => new Uri($"{Host}:{Port}");
+        }
+        
         public string Type { get; } = "Consul";
         public int HeartBeatInterval { get; set; } = 10;
     }
