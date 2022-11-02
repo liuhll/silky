dd74753041b70b344074373e7dc9a546b323c4eb
liuhll <1029765111@qq.com>
2022-11-02 09:42:55 +0800

    移除不必要的名称空间

--- framework/src/Silky.HealthChecks.Rpc/SilkyRpcHealthCheckEndpointRouteBuilderExtensions.cs
+++ framework/src/Silky.HealthChecks.Rpc/SilkyRpcHealthCheckEndpointRouteBuilderExtensions.cs
@@ -1,14 +1,7 @@
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Silky.Core;
-using Silky.Core.Extensions;
-using Silky.Core.Serialization;
 
 namespace Silky.HealthChecks.Rpc
 {
