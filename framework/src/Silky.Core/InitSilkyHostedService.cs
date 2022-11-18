edd57a332d205afd0db9302e953f049ceb8aa669
liuhll <1029765111@qq.com>
2022-11-18 17:52:57 +0800

    移除不必要的名称空间

--- framework/src/Silky.Core/InitSilkyHostedService.cs
+++ framework/src/Silky.Core/InitSilkyHostedService.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Silky.Core.Modularity;
-using Silky.Core.Utils;
 
 namespace Silky.Core
 {
