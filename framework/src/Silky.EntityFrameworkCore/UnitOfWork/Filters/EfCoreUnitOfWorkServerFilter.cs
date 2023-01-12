9b5a0775fb1eed7ee8bf858117b65543fc7d132e
liuhll <1029765111@qq.com>
2023-01-12 10:21:49 +0800

    优化EfCore工作单元过滤器

--- framework/src/Silky.EntityFrameworkCore/UnitOfWork/Filters/EfCoreUnitOfWorkServerFilter.cs
+++ framework/src/Silky.EntityFrameworkCore/UnitOfWork/Filters/EfCoreUnitOfWorkServerFilter.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using Silky.Core;
 using Silky.Core.DbContext;
 using Silky.Core.DbContext.UnitOfWork;
 using Silky.Core.Extensions;
@@ -9,48 +7,44 @@ using Silky.Rpc.Filters;
 
 namespace Silky.EntityFrameworkCore.UnitOfWork
 {
-    public class EfCoreUnitOfWorkServerFilter : IAsyncServerFilter, IOrderedFilter
+    public class EfCoreUnitOfWorkServerFilter : IAsyncServerFilter
     {
         private readonly ISilkyDbContextPool _silkyDbContextPool;
-        private UnitOfWorkAttribute _unitOfWorkAttribute;
-        private bool _isManualSaveChanges;
 
         public EfCoreUnitOfWorkServerFilter(ISilkyDbContextPool silkyDbContextPool)
         {
             _silkyDbContextPool = silkyDbContextPool;
         }
 
-        public int Order { get; } = Int32.MaxValue - 1;
-        
         public async Task OnActionExecutionAsync(ServerInvokeExecutingContext context, ServerExecutionDelegate next)
         {
             var instanceMethod = context.InstanceType?.GetCompareMethod(context.ServiceEntry.MethodInfo,
                 context.ServiceEntry.MethodInfo.Name);
-            _unitOfWorkAttribute =
+            var unitOfWorkAttribute =
                 instanceMethod?.GetCustomAttributes(true).OfType<UnitOfWorkAttribute>().FirstOrDefault();
 
-            _isManualSaveChanges = context.ServiceEntry.CustomAttributes.OfType<ManualCommitAttribute>().Any();
+            var isManualSaveChanges = context.ServiceEntry.CustomAttributes.OfType<ManualCommitAttribute>().Any();
 
-            if (_unitOfWorkAttribute != null)
+            if (unitOfWorkAttribute != null)
             {
-                _silkyDbContextPool?.BeginTransaction(_unitOfWorkAttribute.EnsureTransaction);
+                _silkyDbContextPool.BeginTransaction(unitOfWorkAttribute.EnsureTransaction);
             }
-
             var result = await next();
             try
             {
-                if (_unitOfWorkAttribute == null)
+                if (unitOfWorkAttribute == null)
                 {
-                    if (result.Exception == null && !_isManualSaveChanges) _silkyDbContextPool?.SavePoolNow();
+                    if (result.Exception == null && !isManualSaveChanges)
+                        await _silkyDbContextPool.SavePoolNowAsync();
                 }
                 else
                 {
-                    _silkyDbContextPool?.CommitTransaction(_isManualSaveChanges, result.Exception);
+                    _silkyDbContextPool.CommitTransaction(isManualSaveChanges, result.Exception);
                 }
             }
             finally
             {
-                _silkyDbContextPool?.CloseAll();
+                _silkyDbContextPool.CloseAll();
             }
         }
     }
