34ef645fcd0952339120fc69a5e57856b764724a
liuhll <1029765111@qq.com>
2023-10-15 11:59:49 +0800

    修复心跳适配器存在的bug

--- framework/src/Silky.DotNetty.Abstraction/Handlers/ChannelInboundHandlerAdapter.cs
+++ framework/src/Silky.DotNetty.Abstraction/Handlers/ChannelInboundHandlerAdapter.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Net;
 using DotNetty.Buffers;
 using DotNetty.Handlers.Timeout;
 using DotNetty.Transport.Channels;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Silky.Core.Extensions;
 using Silky.Core.Runtime.Rpc;
 using Silky.DotNetty.Abstraction;
@@ -13,35 +16,55 @@ namespace Silky.DotNetty.Handlers
     {
         private readonly IRpcEndpointMonitor _rpcEndpointMonitor;
 
+        public ILogger<ChannelInboundHandlerAdapter> Logger { get; set; }
+
         public ChannelInboundHandlerAdapter(IRpcEndpointMonitor rpcEndpointMonitor)
         {
             _rpcEndpointMonitor = rpcEndpointMonitor;
+            Logger = NullLogger<ChannelInboundHandlerAdapter>.Instance;
         }
 
         public ChannelInboundHandlerAdapter()
         {
+            Logger = NullLogger<ChannelInboundHandlerAdapter>.Instance;
         }
 
         public override async void UserEventTriggered(IChannelHandlerContext context, object evt)
         {
-            if (evt is IdleStateEvent { State: IdleState.ReaderIdle })
+            try
             {
-                if (context.Channel.RemoteAddress is IPEndPoint remoteAddress)
+                if (evt is IdleStateEvent { State: IdleState.ReaderIdle })
                 {
-                    _rpcEndpointMonitor?.ChangeStatus(remoteAddress.Address.MapToIPv4(),
-                        remoteAddress.Port,
-                        ServiceProtocol.Rpc,
-                        false);
+                    if (context.Channel.RemoteAddress is IPEndPoint remoteAddress)
+                    {
+                        _rpcEndpointMonitor?.ChangeStatus(remoteAddress.Address.MapToIPv4(),
+                            remoteAddress.Port,
+                            ServiceProtocol.Rpc,
+                            false);
+                    }
+                }
+
+                if (evt is IdleStateEvent { State: IdleState.WriterIdle })
+                {
+                    var buffer = Unpooled.WrappedBuffer(HeartBeat.Semaphore.GetBytes());
+                    await context.Channel.WriteAndFlushAsync(buffer);
+                }
+                else
+                {
+                    base.UserEventTriggered(context, evt);
                 }
             }
-            if (evt is IdleStateEvent { State: IdleState.WriterIdle })
+            catch (Exception e)
             {
-                var buffer = Unpooled.WrappedBuffer(HeartBeat.Semaphore.GetBytes());
-                await context.Channel.WriteAndFlushAsync(buffer);
-            }
-            else
-            {
-                base.UserEventTriggered(context, evt);
+                Logger.LogError(e.Message, e);
+                try
+                {
+                    await context.CloseAsync();
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError(ex.Message, ex);
+                }
             }
         }
 
